ec87f61ace550ddf61ed288ebc53a61b77eb0020
Nick Rodriguez <nrodrig1@gmail.com>
2024-08-16 08:46:28 +0200

    Initial Commit

---
+++ LocationFetcher02/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LocationFetcher02
+//
+//  Created by Nick Rodriguez on 16/08/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
